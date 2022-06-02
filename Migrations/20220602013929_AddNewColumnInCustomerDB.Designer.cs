ee6eaab3ecadbf764dda2dfaad384b5fe1dcce0a
Thaina Biudes <thaina.biudes@gmail.com>
2022-06-01 22:48:58 -0300

    feat: atualiza migrations

--- Migrations/20220602013929_AddNewColumnInCustomerDB.Designer.cs
+++ Migrations/20220602013929_AddNewColumnInCustomerDB.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Customers.API.Model.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Customers5._0.Migrations
 {
     [DbContext(typeof(MySQLContext))]
-    partial class MySQLContextModelSnapshot : ModelSnapshot
+    [Migration("20220602013929_AddNewColumnInCustomerDB")]
+    partial class AddNewColumnInCustomerDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20220602013929_AddNewColumnInCustomerDB.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Customers5._0.Migrations
+{
+    public partial class AddNewColumnInCustomerDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "email",
+                table: "customer",
+                type: "varchar(150)",
+                maxLength: 150,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "customer",
+                keyColumn: "id",
+                keyValue: 1L,
+                column: "email",
+                value: "thaina.biudes@gmail.com");
+
+            migrationBuilder.UpdateData(
+                table: "customer",
+                keyColumn: "id",
+                keyValue: 2L,
+                column: "email",
+                value: "ina_biudes@hotmail.com");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "email",
+                table: "customer");
+        }
+    }
+}
